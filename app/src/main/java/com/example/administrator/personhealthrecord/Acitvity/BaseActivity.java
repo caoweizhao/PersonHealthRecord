656a6793f4821ab9df61fadd6ef1454080bcbc0f
Andy-86 <2660889125@qq.com>
2017-07-18 11:29:02 +0800

    base

---
+++ app/src/main/java/com/example/administrator/personhealthrecord/Acitvity/BaseActivity.java
@@ -0,0 +1,63 @@
+package com.example.administrator.personhealthrecord.Acitvity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.example.administrator.personhealthrecord.Application.MyApplication;
+
+/**
+ * Created by andy on 2017/7/18.
+ */
+
+public class BaseActivity extends Activity{
+    protected final String TAG = getClass().getSimpleName();
+    public static BaseActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        // TODO: 16/9/1  add the third service. eg.umeng ...
+        activity = this;
+        ((MyApplication) MyApplication.getContext()).addActivity(this);
+        init();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activity = this;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activity = null;
+    }
+
+    private void init() {
+        initData();
+        initEvents();
+    }
+
+    /***
+     * 初始化事件（监听事件等事件绑定）
+     */
+    protected void initEvents() {
+    }
+
+    /**
+     * 绑定数据
+     */
+    protected void initData() {
+    }
+
+    /**
+     * activity退出时将activity移出栈
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ((MyApplication) MyApplication.getContext()).removeActivity(this);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/personhealthrecord/Util/ToastUitl.java
@@ -0,0 +1,16 @@
+package com.example.administrator.personhealthrecord.Util;
+
+import android.widget.Toast;
+
+import com.example.administrator.personhealthrecord.Application.MyApplication;
+
+/**
+ * Created by andy on 2017/7/18.
+ */
+
+public class ToastUitl {
+    public static void Toast(String msg)
+    {
+        Toast.makeText(MyApplication.getContext(),msg,Toast.LENGTH_LONG).show();
+    }
+}
