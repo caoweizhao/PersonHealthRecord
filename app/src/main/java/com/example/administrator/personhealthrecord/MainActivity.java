b4240bfa5d37415996dbc4ccfd2637b034697c03
Andy-86 <2660889125@qq.com>
2017-07-18 09:54:47 +0800

    修改TextView

--- app/src/main/java/com/example/administrator/personhealthrecord/MainActivity.java
+++ app/src/main/java/com/example/administrator/personhealthrecord/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.administrator.personhealthrecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
