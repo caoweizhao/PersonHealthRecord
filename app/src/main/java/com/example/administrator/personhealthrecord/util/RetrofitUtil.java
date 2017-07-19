a364340c3c8fb8bec36171b2cd0e5628135c69a1
caoweizhao <devimpl0706@gmail.com>
2017-07-19 20:05:40 +0800

    RetrofitUtil

---
+++ app/src/main/java/com/example/administrator/personhealthrecord/util/RetrofitUtil.java
@@ -0,0 +1,59 @@
+package com.example.administrator.personhealthrecord.util;
+
+import android.util.Log;
+
+import com.example.administrator.personhealthrecord.application.MyApplication;
+import com.example.administrator.personhealthrecord.contract.Contract;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Administrator on 2017-7-19.
+ */
+
+public class RetrofitUtil {
+    private RetrofitUtil() {
+    }
+
+    private static volatile Retrofit mRetrofit;
+    private static File cacheDir = MyApplication.getContext().getCacheDir();
+    private static File cacheFile = new File(cacheDir, "cache");
+    private static HttpLoggingInterceptor mHttpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+        @Override
+        public void log(String message) {
+            Log.d("RetrofitUtil", "message:" + message);
+        }
+    });
+
+    private static long fileSize = 10 * 1024 * 1024; //10MB
+
+    private static OkHttpClient mOkHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(8, TimeUnit.SECONDS)
+            .addInterceptor(mHttpLoggingInterceptor)
+            .cache(new Cache(cacheFile, fileSize))
+            .build();
+
+    public static Retrofit getRetrofit() {
+        if (mRetrofit == null) {
+            synchronized (RetrofitUtil.class) {
+                if (mRetrofit == null) {
+                    mRetrofit = new Retrofit.Builder()
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                            .client(mOkHttpClient)
+                            .baseUrl(Contract.BASE_URL)
+                            .build();
+                }
+            }
+        }
+        return mRetrofit;
+    }
+}
