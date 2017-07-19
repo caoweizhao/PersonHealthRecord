1cebe17e990f3aff0d33a92ab146a4885ca5ac8d
Andy-86 <2660889125@qq.com>
2017-07-19 09:21:30 +0800

    Merge branch 'master' into andy
         # Conflicts:
    two package

---
+++ app/src/main/java/com/example/administrator/personhealthrecord/Adapter/Adapter.java
@@ -0,0 +1,8 @@
+package com.example.administrator.personhealthrecord.Adapter;
+
+/**
+ * Created by andy on 2017/7/19.
+ */
+
+public class Adapter {
+}
---
+++ app/src/main/java/com/example/administrator/personhealthrecord/View/View.java
@@ -0,0 +1,8 @@
+package com.example.administrator.personhealthrecord.View;
+
+/**
+ * Created by andy on 2017/7/19.
+ */
+
+public class View {
+}
