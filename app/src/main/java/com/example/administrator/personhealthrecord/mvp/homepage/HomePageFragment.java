c6d5e85675a2849be04dc9d021cff4410959675e
Andy-86 <2660889125@qq.com>
2017-07-26 09:21:09 +0800

    Merge branch 'master' into andy
                   # Please enter a commit message to explain why this merge is necessary,
                   # especially if it merges an updated upstream into a topic branch.
                   #
                   # Lines starting with '#' will be ignored, and an empty message aborts
                   # the commit.
                   地图

--- app/src/main/java/com/example/administrator/personhealthrecord/mvp/homepage/HomePageFragment.java
+++ app/src/main/java/com/example/administrator/personhealthrecord/mvp/homepage/HomePageFragment.java
@@ -1,6 +1,7 @@
 package com.example.administrator.personhealthrecord.mvp.homepage;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -25,6 +26,7 @@ import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.administrator.personhealthrecord.R;
+import com.example.administrator.personhealthrecord.activity.MapAcitvity;
 import com.example.administrator.personhealthrecord.adapter.HospitalAdapter;
 import com.example.administrator.personhealthrecord.bean.ExpertBean;
 import com.example.administrator.personhealthrecord.bean.HospitalBean;
@@ -69,7 +71,8 @@ public class HomePageFragment extends AHomePageFragment {
 
     @BindView(R.id.near_by_hospital)
     CardView mCardView;
-
+    @BindView(R.id.map_text_view)
+    TextView mpaTextView;
     public HomePageFragment() {
         // Required empty public constructor
     }
@@ -171,6 +174,13 @@ public class HomePageFragment extends AHomePageFragment {
 
     @Override
     protected void initEvent() {
+        mpaTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), MapAcitvity.class);
+                startActivity(intent);
+            }
+        });
         mExpertTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -189,7 +199,6 @@ public class HomePageFragment extends AHomePageFragment {
     protected void initData() {
         mPresenter.onRequestData();
     }
-
     private void initBanner() {
         //设置图片加载器
         mImageBanner.setImageLoader(new GlideImageLoader());
